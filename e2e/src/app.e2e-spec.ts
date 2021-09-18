37575e0deef5737ab6c5db1f498e5ac09a0fd291
tercel_s <0x12f345c+github@gmail.com>
2021-09-19 04:42:44 +0900

    プロジェクト名をリネーム

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('quickstart app is running!');
+    expect(await page.getTitleText()).toEqual('flexGridDynamicResizeSample app is running!');
   });
 
   afterEach(async () => {
